d53d72d4299d7d21602a3352a389b4f3694aa4a5
SozaPidgeon <pidgeon.soren@gmail.com>
2020-03-13 08:47:03 +0800

    2007 files

---
+++ ngoyena.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Soren Pidgeon</title>
+    <meta name="Description" content="This page contains work done by portrait and model photographer Soren Pidgeon, located in Perth, Western Australia. Available for travel."/>
+    <meta name="google-site-verification" content="wv19n4fBlrm0stqhYC1xL-Y5MJ2oswTYnJdv1rs2uN4" />
+    <meta name="theme-color" content="#000000"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta property="og:title" content="Soren Pidgeon"/>
+    <meta property="keywords" content="soren, pidgeon, photography, photographer, perth, western, australia, model, fashion, creative"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://www.sorenpidgeon.com" />
+    <meta property="og:image" content="https://www.sorenpidgeon.com/pictures/meandher.JPG"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="2007style.css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow:700" rel="stylesheet" defer>
+    <link rel="stylesheet" href="bs.css" defer/>
+    <link rel="stylesheet" href="bs-grid.css" defer/>
+    <link rel="shortcut icon" href="me.ico"/>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <!-- Contents -->
+    <div id="zine2007contents">
+        <video height="75%" width="75%" src="2007/ngoyena.MP4" alt="nothing" autoplay loop controls>
+    </div>
+
+</body>
+</html>
